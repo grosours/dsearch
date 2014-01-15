9023acfb5718307660d866c7a37c9f928547163d
Guillaume Faure <guillaume.faure@visuamobile.com>
2014-01-15 13:37:24 +0100

    Implemente le model des albums.
    Signed-off-by: Guillaume Faure <guillaume.faure@visuamobile.com>

--- dsearch/DSRAlbum.m
+++ dsearch/DSRAlbum.m
@@ -7,7 +7,38 @@
 //
 
 #import "DSRAlbum.h"
+#import "DSRObject.h"
 
 @implementation DSRAlbum
+- (NSSet *)supportedKeys
+{
+    return [[super supportedKeys]
+            setByAddingObjectsFromArray:@[@"title", @"upc", @"link", @"cover",
+                                          @"genre_id", @"label", @"nb_tracks",
+                                          @"duration", @"fans", @"rating", @"release_date",
+                                          @"available", @"artist", @"tracks"]];
+}
 
+- (NSSet*)supportedMethods
+{
+    return [[super supportedMethods]
+            setByAddingObjectsFromArray:@[@"tracks"]];
+}
+
+- (NSString *)infoURL
+{
+    return [NSString stringWithFormat:@"http://api.deezer.com/album/%@", [self valueForKeyPath:@"info.id"]];
+}
+
+#pragma mark Parsing
+
+- (id)parseArtist:(NSDictionary*)JSON
+{
+    return [DSRObject objectFromJSON:JSON];
+}
+
+- (id)parseTracks:(NSDictionary*)JSON
+{
+    return [DSRObject objectsFromJSON:JSON];
+}
 @end
